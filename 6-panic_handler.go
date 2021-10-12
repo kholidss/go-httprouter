e40e367f0c48ecafe23f843ecb89452c41e46e85
kholid saifulloh <muhammadkholid@refactory.id>
2021-10-12 14:03:12 +0700

    add panic handler

---
+++ 6-panic_handler.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/julienschmidt/httprouter"
+)
+
+func Hello(rw http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	fmt.Fprint(rw, "this is from hello")
+}
+
+func main() {
+	router := httprouter.New()
+
+	router.PanicHandler = func(rw http.ResponseWriter, r *http.Request, error interface{}) {
+		fmt.Fprintf(rw, "Panic: ", error)
+	}
+
+	//with anonymous functiom
+	router.GET("/", func(rw http.ResponseWriter, r *http.Request, p httprouter.Params) {
+		panic("ups")
+	})
+
+	//with call the function
+	router.GET("/hello", Hello)
+
+	server := http.Server{
+		Handler: router,
+		Addr:    "localhost:3000",
+	}
+
+	server.ListenAndServe()
+}
